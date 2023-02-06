5543795a36a9390731ba5e87612ea89da83f58ec
gr402_hudal <dimahudobin05@gmail.com>
2023-02-06 10:35:55 +0700

    ttit5

--- js/main.js
+++ js/main.js
@@ -60,25 +60,28 @@ Vue.component('cards-kanban', {
 })
 
 Vue.component('fill', {     //содержит:дата создания, заголовок, описание задачи, дедлайн
-    props:{
-
-    },
     template:`
-    <div id="form">
-        <h3>Заполните карточку задачи</h3>
-        <form @submit.prevent="onSubmit">
-            <p class="pForm">Введите заголовок: 
-                <input required type="text" v-model="title" maxlength="30" placeholder="Заголовок">
-            </p>
-            <p class="pForm">Добавьте описание задаче:</p>
-            <textarea v-model="description" cols="40" rows="4"></textarea>
-            <p class="pForm">Укажите дату дедлайна: 
-                <input required type="date" v-model="dateD">
-            </p>
-            <p class="pForm">
-                <input class="button" type="submit" value="Добвить задачу">
-            </p>
-        </form>
+    <div>
+        <button v-if="!show" @click="openModal">Добавьте задачу</button>
+        <div id="form" v-if="show" class="modal-shadow">
+            <div class="modal">
+                <div class="modal-close" @click="closeModal">&#10006;</div>
+                <h3>Заполните карточку задачи</h3>
+                <form @submit.prevent="onSubmit">
+                    <p class="pForm">Введите заголовок: 
+                        <input required type="text" v-model="title" maxlength="30" placeholder="Заголовок">
+                    </p>
+                    <p class="pForm">Добавьте описание задаче:</p>
+                    <textarea v-model="description" cols="40" rows="4"></textarea>
+                    <p class="pForm">Укажите дату дедлайна: 
+                        <input required type="date" v-model="dateD">
+                    </p>
+                    <p class="pForm">
+                        <input class="button" type="submit" value="Добвить задачу">
+                    </p>
+                </form>
+            </div>
+        </div>    
     </div>
     `,
     data(){
@@ -86,6 +89,7 @@ Vue.component('fill', {     //содержит:дата создания, заг
             title: null,
             description: null,
             dateD: null,
+            show: false
         }
     },
     methods: {
@@ -105,8 +109,15 @@ Vue.component('fill', {     //содержит:дата создания, заг
             this.title = null
             this.description = null
             this.dateD = null
+            this.closeModal()
             console.log(card)
         },
+        closeModal(){
+            this.show = false
+        },
+        openModal(){
+            this.show = true
+        }
     }
 })
 
